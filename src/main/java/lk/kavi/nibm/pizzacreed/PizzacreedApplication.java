f2475f5309da415fe014328ebe6632b4deefd761
lafesto <kavishkadinajara@gmail.com>
2024-05-12 22:40:38 +0530

    first commit

---
+++ src/main/java/lk/kavi/nibm/pizzacreed/PizzacreedApplication.java
@@ -0,0 +1,13 @@
+package lk.kavi.nibm.pizzacreed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzacreedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzacreedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/kavi/nibm/pizzacreed/PizzacreedApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.kavi.nibm.pizzacreed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzacreedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
